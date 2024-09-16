53cb13c58cb26672847027113ec158ceb2bb95d1
Shashank Arjun Ingle <shashank227421@gmail.com>
2024-09-17 00:19:48 +0530

    binaryto decimal

--- binaryTodecimail1.cpp
+++ binaryTodecimail1.cpp
@@ -6,15 +6,24 @@ class conversion{
 public: 
         void binarytodecimal(int n)
         {
+            int ans=0;
            for (int i = 0; n!=0; i++)
            {
             
+            int rem = n % 10;
+            
+            ans= (rem * pow(2,i))+ans;
+            n=n/10;
            }
-           
+           cout<<ans<<" ";
         }
 };
 int main()
 {
+    int num;
+    cin >>num;
+    conversion c1;
+    c1.binarytodecimal(num);
 
     return 0;
 }
