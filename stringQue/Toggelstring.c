f71b51b9fb252286bf668726ba8f103ad269c9fe
Shashank Arjun Ingle <shashank227421@gmail.com>
2024-09-26 16:34:42 +0530

    Toggelstring.c

--- stringQue/Toggelstring.c
+++ stringQue/Toggelstring.c
@@ -4,16 +4,15 @@ int main()
     char str[50];
     int i=0,n=0;
     printf("Enter the string: ");
-    fgets(str, sizeof(str), stdin);
+    fgets(str, sizeof(str), stdin);// if we are taking input as Sentence from the user then use fgets() function
+    // because if give input "shashank ingle" the it store  "s h a s h a n k   i n g l e " store black space between
+    // while scanf() function can;t storee the sentence it store the word and end work with '\0'
     // printf("%s",str);
     
-    while (str[i]!='\0')
-    {
-        n++;
-        i++;
-    }
+   
   
-    for (int i = 0; str[i]!='\n' ; i++)
+    for (int i = 0; str[i]!='\n' ; i++)// condition till neew line
+    
     {
         if(str[i]>='A' && str[i]<='Z')
         {
