0919a2e9792073a0463821b865226281be285699
Shashank Arjun Ingle <shashank227421@gmail.com>
2024-09-26 19:42:35 +0530

    26sep24

---
+++ stringQue/char*str=string.c
@@ -0,0 +1,32 @@
+
+
+#include<stdio.h>
+
+int main()
+{ int n=0;
+    char *str= "shashank ingle";
+    //1st way to access the each character of char *str="shashank ingle"
+    // for (int i = 0; str[i]!='\0'; i++)
+    // {
+    //     printf("%c ",str[i]);
+    // }
+
+
+    //2nd way to access the each charater of the char *str= "shashank ingle"
+    // for (; *str!='\0'; str++)// no need to initialize the for loop that why we use while loop
+    // {
+    //     printf("%c ",*str);
+    // }
+    while (*str!='\0')
+    {
+        printf("%c ",*str);
+        str++;
+        n++;
+    }
+    
+    // printf("%d",n);
+
+    //main code 
+
+    return 0;
+}
