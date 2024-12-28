520a2ebb2e21fde9630aa3e66bd8dd4fc806d57d
Shashank Arjun Ingle <shashank227421@gmail.com>
2024-12-28 19:25:42 +0530

    28DEC24

---
+++ pattern/pattern4.cpp
@@ -0,0 +1,28 @@
+/*
+pattern
+*
+**
+***
+****
+
+*/
+
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+    int n;
+    cin>>n;
+    for (int i = 1; i <=n; i++)
+    {
+        cout << " ";
+        for (int j = 1; j<=i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+
+    }
+    
+    return 0;
+ }
