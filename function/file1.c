2c8f30fa9b616f5c1099a25bc67b08cd0292b481
Shashank Arjun Ingle <shashank227421@gmail.com>
2024-12-28 19:28:00 +0530

    28DEC24

---
+++ function/file1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// #include"file2.c"
+extern int fun (int a , int b);
+int main()
+{
+    int a1,a2;
+    printf("ENter the numbers\n");
+    scanf("%d %d", &a1,&a2);
+    int result=fun(a1,a2);
+    printf("%d", result);
+    printf("%d %d", a1,a2);
+    return 0;
+}
---
+++ function/file2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+static int fun (int a , int b)
+{
+    // a=10, b=20;
+    int sum= a+b;
+    printf("hello\n");
+    return sum;
+}
